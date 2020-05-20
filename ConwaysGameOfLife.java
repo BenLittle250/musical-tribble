32643a48109083fdb590ede50f57ebce372a1a26
BenLittle250 <61598422+BenLittle250@users.noreply.github.com>
2020-05-20 10:25:13 -0500

    Add files via upload

---
+++ ConwaysGameOfLife.java
@@ -0,0 +1,14 @@
+package postAP;
+
+public class ConwaysGameOfLife {
+
+	public static void main(String[] args) {
+		System.out.println(plus3(2));
+
+	}
+	
+	public static int plus3 (int x) {
+		return x + 3;
+	}
+
+}
